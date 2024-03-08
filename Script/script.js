381e3c3ee57cd6f570e93c64af2e62c64d85e22f
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-03-09 03:40:58 +0700

    Update Code

---
+++ Script/script.js
@@ -0,0 +1,15 @@
+function openlogin() {
+    document.getElementById("login").style.display = "block";
+}
+
+function closelogin() {
+    document.getElementById("login").style.display = "none";
+}
+
+function openSignup() {
+    document.getElementById("signup").style.display = "block";
+}
+
+function closeSignup() {
+    document.getElementById("signup").style.display = "none";
+}
